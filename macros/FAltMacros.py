e3da0f087fbbf6c620c2ecf38690cf4303e7710b
IlyaChaban <ilyachaban@gmail.com>
2022-09-04 17:33:02 +0200

    adding F macros and Alt+F macros

---
+++ macros/FAltMacros.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: 2021 Phillip Burgess for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+# MACROPAD Hotkeys example: Mouse control
+
+# The syntax for Mouse macros is highly peculiar, in order to maintain
+# backward compatibility with the original keycode-only macro files.
+# The third item for each macro is a list in brackets, and each value within
+# is normally an integer (Keycode), float (delay) or string (typed literally).
+# Consumer Control codes were added as list-within-list, and then mouse
+# further complicates this by adding dicts-within-list. Each mouse-related
+# dict can have any mix of keys 'buttons' w/integer mask of button values
+# (positive to press, negative to release), 'x' w/horizontal motion,
+# 'y' w/vertical and 'wheel' with scrollwheel motion.
+
+# To reference Mouse constants, import Mouse like so...
+from adafruit_hid.keycode import Keycode
+# You can still import Keycode and/or ConsumerControl as well if a macro file
+# mixes types! See other macro files for typical Keycode examples.
+
+app = {               # REQUIRED dict, must be named 'app'
+    'name' : 'Alt+F macros', # Application name
+    'macros' : [      # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x000500, 'Alt+F13', [Keycode.ALT, Keycode.F13]),
+        (0x000500, 'Alt+F14', [Keycode.ALT, Keycode.F14]),
+        (0x000500, 'Alt+F15', [Keycode.ALT, Keycode.F15]),
+        # 2nd row ----------
+        (0x000500, 'Alt+F16', [Keycode.ALT, Keycode.F16]),
+        (0x000500, 'Alt+F17', [Keycode.ALT, Keycode.F17]),
+        (0x000500, 'Alt+F18', [Keycode.ALT, Keycode.F18]),
+        # 3rd row ----------
+        (0x000500, 'Alt+F19', [Keycode.ALT, Keycode.F19]),
+        (0x000500, 'Alt+F20', [Keycode.ALT, Keycode.F20]),
+        (0x000500, 'Alt+F21', [Keycode.ALT, Keycode.F21]),
+        # 4th row ----------
+        (0x000500, 'Alt+F22', [Keycode.ALT, Keycode.F22]),
+        (0x000500, 'Alt+F23', [Keycode.ALT, Keycode.F23]),
+        (0x000500, 'Alt+F24', [Keycode.ALT, Keycode.F24]),
+        # Encoder button ---
+        (0x000000, '', [])
+        ],
+    
+    'icons' : 'AltF_macros.bmp'
+}
